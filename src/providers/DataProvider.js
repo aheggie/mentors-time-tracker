487a08f101055e41588b67ec792a24fa293cb791
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-19 01:40:01 -0500

    basic data provider structured - but not tested

--- src/providers/DataProvider.js
+++ src/providers/DataProvider.js
@@ -1,1 +1,21 @@
 import { useState, createContext } from "react";
+
+export const HoursContext = createContext();
+
+const HoursStateProvider = ({ children }) => {
+  const [currentTimeSeries, setTimeSeries] = useState("hours");
+
+  const updateTime = () => setTimeSeries("days");
+  return (
+    <HoursContext.Provider
+      value={{
+        currentTimeSeries: currentTimeSeries,
+        updateTime: updateTime,
+      }}
+    >
+      {children}
+    </HoursContext.Provider>
+  );
+};
+
+export default HoursStateProvider;
