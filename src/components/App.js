8f2c5edf21c8455f9e8b18f49f944abaeb4be841
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-18 21:35:49 -0500

    making .cards-container the render component fixes things in css

--- src/components/App.js
+++ src/components/App.js
@@ -2,174 +2,172 @@ import UserCard from "./UserCard";
 
 const App = () => (
   <>
-    <div class="cards-container">
-      <UserCard />
-      <div class="card card--work">
-        <div class="card__topaccent">
-          <img
-            src="./img/icon-work.svg"
-            alt="accent icon"
-            class="card__topaccentimg"
-          />
+    <UserCard />
+    <div class="card card--work">
+      <div class="card__topaccent">
+        <img
+          src="./img/icon-work.svg"
+          alt="accent icon"
+          class="card__topaccentimg"
+        />
+      </div>
+      <div class="card__maincard">
+        <div class="card__titlebar">
+          <h2 class="card__title">Work</h2>
+          <button class="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt="ellipsis icon"
+              class="card__ellipsisicon"
+            />
+          </button>
         </div>
-        <div class="card__maincard">
-          <div class="card__titlebar">
-            <h2 class="card__title">Work</h2>
-            <button class="card__menubutton">
-              <img
-                src="./img/icon-ellipsis.svg"
-                alt="ellipsis icon"
-                class="card__ellipsisicon"
-              />
-            </button>
-          </div>
-          <div class="card__stats-container">
-            <h3 class="card__hours">10hrs</h3>
-            <small class="card__pastinfo">
-              Last <span class="card__pasttimeperiod">Week</span> -
-              <span class="card__pasthours">8hrs</span>
-            </small>
-          </div>
+        <div class="card__stats-container">
+          <h3 class="card__hours">10hrs</h3>
+          <small class="card__pastinfo">
+            Last <span class="card__pasttimeperiod">Week</span> -
+            <span class="card__pasthours">8hrs</span>
+          </small>
         </div>
       </div>
-      <div class="card card--play">
-        <div class="card__topaccent">
-          <img
-            src="./img/icon-work.svg"
-            alt="accent icon"
-            class="card__topaccentimg"
-          />
+    </div>
+    <div class="card card--play">
+      <div class="card__topaccent">
+        <img
+          src="./img/icon-work.svg"
+          alt="accent icon"
+          class="card__topaccentimg"
+        />
+      </div>
+      <div class="card__maincard">
+        <div class="card__titlebar">
+          <h2 class="card__title">Plork</h2>
+          <button class="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt="ellipsis icon"
+              class="card__ellipsisicon"
+            />
+          </button>
         </div>
-        <div class="card__maincard">
-          <div class="card__titlebar">
-            <h2 class="card__title">Plork</h2>
-            <button class="card__menubutton">
-              <img
-                src="./img/icon-ellipsis.svg"
-                alt="ellipsis icon"
-                class="card__ellipsisicon"
-              />
-            </button>
-          </div>
-          <div class="card__stats-container">
-            <h3 class="card__hours">10hrs</h3>
-            <small class="card__pastinfo">
-              Last <span class="card__pasttimeperiod">Week</span> -
-              <span class="card__pasthours">8hrs</span>
-            </small>
-          </div>
+        <div class="card__stats-container">
+          <h3 class="card__hours">10hrs</h3>
+          <small class="card__pastinfo">
+            Last <span class="card__pasttimeperiod">Week</span> -
+            <span class="card__pasthours">8hrs</span>
+          </small>
         </div>
       </div>
-      <div class="card card--study">
-        <div class="card__topaccent">
-          <img
-            src="./img/icon-study.svg"
-            alt="accent icon"
-            class="card__topaccentimg"
-          />
+    </div>
+    <div class="card card--study">
+      <div class="card__topaccent">
+        <img
+          src="./img/icon-study.svg"
+          alt="accent icon"
+          class="card__topaccentimg"
+        />
+      </div>
+      <div class="card__maincard">
+        <div class="card__titlebar">
+          <h2 class="card__title">Work</h2>
+          <button class="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt="ellipsis icon"
+              class="card__ellipsisicon"
+            />
+          </button>
         </div>
-        <div class="card__maincard">
-          <div class="card__titlebar">
-            <h2 class="card__title">Work</h2>
-            <button class="card__menubutton">
-              <img
-                src="./img/icon-ellipsis.svg"
-                alt="ellipsis icon"
-                class="card__ellipsisicon"
-              />
-            </button>
-          </div>
-          <div class="card__stats-container">
-            <h3 class="card__hours">10hrs</h3>
-            <small class="card__pastinfo">
-              Last <span class="card__pasttimeperiod">Week</span> -
-              <span class="card__pasthours">8hrs</span>
-            </small>
-          </div>
+        <div class="card__stats-container">
+          <h3 class="card__hours">10hrs</h3>
+          <small class="card__pastinfo">
+            Last <span class="card__pasttimeperiod">Week</span> -
+            <span class="card__pasthours">8hrs</span>
+          </small>
         </div>
       </div>
-      <div class="card card--exercise">
-        <div class="card__topaccent">
-          <img
-            src="./img/icon-exercise.svg"
-            alt="accent icon"
-            class="card__topaccentimg"
-          />
+    </div>
+    <div class="card card--exercise">
+      <div class="card__topaccent">
+        <img
+          src="./img/icon-exercise.svg"
+          alt="accent icon"
+          class="card__topaccentimg"
+        />
+      </div>
+      <div class="card__maincard">
+        <div class="card__titlebar">
+          <h2 class="card__title">Work</h2>
+          <button class="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt="ellipsis icon"
+              class="card__ellipsisicon"
+            />
+          </button>
         </div>
-        <div class="card__maincard">
-          <div class="card__titlebar">
-            <h2 class="card__title">Work</h2>
-            <button class="card__menubutton">
-              <img
-                src="./img/icon-ellipsis.svg"
-                alt="ellipsis icon"
-                class="card__ellipsisicon"
-              />
-            </button>
-          </div>
-          <div class="card__stats-container">
-            <h3 class="card__hours">10hrs</h3>
-            <small class="card__pastinfo">
-              Last <span class="card__pasttimeperiod">Week</span> -
-              <span class="card__pasthours">8hrs</span>
-            </small>
-          </div>
+        <div class="card__stats-container">
+          <h3 class="card__hours">10hrs</h3>
+          <small class="card__pastinfo">
+            Last <span class="card__pasttimeperiod">Week</span> -
+            <span class="card__pasthours">8hrs</span>
+          </small>
         </div>
       </div>
-      <div class="card card--social">
-        <div class="card__topaccent">
-          <img
-            src="./img/icon-social.svg"
-            alt="accent icon"
-            class="card__topaccentimg"
-          />
+    </div>
+    <div class="card card--social">
+      <div class="card__topaccent">
+        <img
+          src="./img/icon-social.svg"
+          alt="accent icon"
+          class="card__topaccentimg"
+        />
+      </div>
+      <div class="card__maincard">
+        <div class="card__titlebar">
+          <h2 class="card__title">Work</h2>
+          <button class="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt="ellipsis icon"
+              class="card__ellipsisicon"
+            />
+          </button>
         </div>
-        <div class="card__maincard">
-          <div class="card__titlebar">
-            <h2 class="card__title">Work</h2>
-            <button class="card__menubutton">
-              <img
-                src="./img/icon-ellipsis.svg"
-                alt="ellipsis icon"
-                class="card__ellipsisicon"
-              />
-            </button>
-          </div>
-          <div class="card__stats-container">
-            <h3 class="card__hours">10hrs</h3>
-            <small class="card__pastinfo">
-              Last <span class="card__pasttimeperiod">Week</span> -
-              <span class="card__pasthours">8hrs</span>
-            </small>
-          </div>
+        <div class="card__stats-container">
+          <h3 class="card__hours">10hrs</h3>
+          <small class="card__pastinfo">
+            Last <span class="card__pasttimeperiod">Week</span> -
+            <span class="card__pasthours">8hrs</span>
+          </small>
         </div>
       </div>
-      <div class="card card--self-care">
-        <div class="card__topaccent">
-          <img
-            src="./img/icon-self-care.svg"
-            alt="accent icon"
-            class="card__topaccentimg"
-          />
+    </div>
+    <div class="card card--self-care">
+      <div class="card__topaccent">
+        <img
+          src="./img/icon-self-care.svg"
+          alt="accent icon"
+          class="card__topaccentimg"
+        />
+      </div>
+      <div class="card__maincard">
+        <div class="card__titlebar">
+          <h2 class="card__title">Work</h2>
+          <button class="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt="ellipsis icon"
+              class="card__ellipsisicon"
+            />
+          </button>
         </div>
-        <div class="card__maincard">
-          <div class="card__titlebar">
-            <h2 class="card__title">Work</h2>
-            <button class="card__menubutton">
-              <img
-                src="./img/icon-ellipsis.svg"
-                alt="ellipsis icon"
-                class="card__ellipsisicon"
-              />
-            </button>
-          </div>
-          <div class="card__stats-container">
-            <h3 class="card__hours">10hrs</h3>
-            <small class="card__pastinfo">
-              Last <span class="card__pasttimeperiod">Week</span> -
-              <span class="card__pasthours">8hrs</span>
-            </small>
-          </div>
+        <div class="card__stats-container">
+          <h3 class="card__hours">10hrs</h3>
+          <small class="card__pastinfo">
+            Last <span class="card__pasttimeperiod">Week</span> -
+            <span class="card__pasthours">8hrs</span>
+          </small>
         </div>
       </div>
     </div>
