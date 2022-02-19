3f67eb8ea860726111f2e89858aafb4cb2aeab7e
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-19 01:50:16 -0500

    context received by UserCard'

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,24 +1,35 @@
-const UserCard = () => (
-  <div className="usercard">
-    <div className="usercard__userinfo">
-      <img
-        src="./img/image-jeremy.png"
-        alt=""
-        className="usercard__avatarimg"
-      />
-      <div className="usercard__name-container">
-        <small className="usercard__reportfor">Report For</small>
-        <h1 className="usercard__name">Jeremy Robson</h1>
+import { useContext } from "react";
+import { HoursContext } from "../providers/HoursStateProvider";
+
+const UserCard = () => {
+  const { currentTimeSeries, updateTime } = useContext(HoursContext);
+
+  console.log(currentTimeSeries);
+
+  return (
+    <div className="usercard">
+      <div className="usercard__userinfo">
+        <img
+          src="./img/image-jeremy.png"
+          alt=""
+          className="usercard__avatarimg"
+        />
+        <div className="usercard__name-container">
+          <small className="usercard__reportfor">Report For</small>
+          <h1 className="usercard__name">Jeremy Robson</h1>
+        </div>
+      </div>
+      <div className="usercard__navarea">
+        <ul className="usercard__navlist">
+          <li className="usercard__navitem">Daily</li>
+          <li className="usercard__navitem usercard__navitem--active">
+            Weekly
+          </li>
+          <li className="usercard__navitem">Monthly</li>
+        </ul>
       </div>
     </div>
-    <div className="usercard__navarea">
-      <ul className="usercard__navlist">
-        <li className="usercard__navitem">Daily</li>
-        <li className="usercard__navitem usercard__navitem--active">Weekly</li>
-        <li className="usercard__navitem">Monthly</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default UserCard;
