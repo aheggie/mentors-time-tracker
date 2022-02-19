558bf03de985bf24a45c7df2d7b64fbf11ef15f5
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-19 01:45:40 -0500

    provider doesnt break the app test data access next

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import HoursStateProvider from "./providers/HoursStateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HoursStateProvider>
+      <App />
+    </HoursStateProvider>
   </React.StrictMode>,
   document.querySelector(".cards-container")
 );
