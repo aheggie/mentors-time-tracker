f188737b5dadbacd6a135996da6f66af95095ec9
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-19 02:19:53 -0500

    got to google this json error tomorrow

--- src/providers/HoursStateProvider.js
+++ src/providers/HoursStateProvider.js
@@ -4,7 +4,12 @@ export const HoursContext = createContext();
 
 const HoursStateProvider = ({ children }) => {
   const [currentTimeSeries, setTimeSeries] = useState("hours");
-
+  console.log("hello in provider");
+  fetch("data.json")
+    // response has a .json() method but response.json() returns an "Unexpected token < in JSON at position 0" error
+    .then((response) => console.log(response))
+    // .then((data) => console.log(data))
+    .catch((err) => console.error("Error oh no", err));
   const updateTime = () => setTimeSeries("days");
   return (
     <HoursContext.Provider
