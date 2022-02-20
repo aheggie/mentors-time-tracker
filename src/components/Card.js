0d0c78836b70d20309d23afdc518035c42d8f9a4
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-02-19 23:28:21 -0500

    Card is receiving data from context provider

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,36 @@
-const Card = ({ title, modifierClass, imgFile }) => (
-  <div className={`card ${modifierClass}`}>
-    <div className="card__topaccent">
-      <img src={`./img/${imgFile}`} alt="" className="card__topaccentimg" />
-    </div>
-    <div className="card__maincard">
-      <div className="card__titlebar">
-        <h2 className="card__title">{title}</h2>
-        <button className="card__menubutton">
-          <img
-            src="./img/icon-ellipsis.svg"
-            alt=""
-            className="card__ellipsisicon"
-          />
-        </button>
+import { useContext } from "react";
+import { HoursContext } from "../providers/HoursStateProvider";
+
+const Card = ({ title, modifierClass, imgFile }) => {
+  const { processedTimeSeriesData } = useContext(HoursContext);
+
+  console.log(processedTimeSeriesData);
+  return (
+    <div className={`card ${modifierClass}`}>
+      <div className="card__topaccent">
+        <img src={`./img/${imgFile}`} alt="" className="card__topaccentimg" />
       </div>
-      <div className="card__stats-container">
-        <h3 className="card__hours">10hrs</h3>
-        <small className="card__pastinfo">
-          Last <span className="card__pasttimeperiod">Week</span> -
-          <span className="card__pasthours">8hrs</span>
-        </small>
+      <div className="card__maincard">
+        <div className="card__titlebar">
+          <h2 className="card__title">{title}</h2>
+          <button className="card__menubutton">
+            <img
+              src="./img/icon-ellipsis.svg"
+              alt=""
+              className="card__ellipsisicon"
+            />
+          </button>
+        </div>
+        <div className="card__stats-container">
+          <h3 className="card__hours">10hrs</h3>
+          <small className="card__pastinfo">
+            Last <span className="card__pasttimeperiod">Week</span> -
+            <span className="card__pasthours">8hrs</span>
+          </small>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
